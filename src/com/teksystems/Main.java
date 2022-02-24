d16d40a14d9256588ac4503ba643514aa0b31f8e
erikaosterbur <erikaosterbur@gmail.com>
2022-02-24 14:36:12 -0600

    Problem instruction set up

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -3,6 +3,76 @@ package com.teksystems;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        problemOne();
+        problemTwo();
+        problemThree();
+        problemFour();
+        problemFive();
+        problemSix();
+        problemSeven();
+        problemEight();
+        problemNine();
+        problemTen();
     }
+
+    public static void problemOne () {
+        /*Write a program that creates an array of integers with a length of 3.
+        Assign the values 1, 2, and 3 to the indexes. Print out each array element.*/
+    }
+
+    public static void problemTwo () {
+        /*Write a program that returns the middle element in an array.
+        Give the following values to the integer array: {13, 5, 7, 68, 2} and produce the following output: 7 */
+    }
+
+    public static void problemThree () {
+        /*Write a program that creates an array of String type and initializes it with the strings “red”, “green”, “blue” and “yellow”.
+        Print out the array length. Make a copy using the clone( ) method.
+        Use the Arrays.toString( ) method on the new array to verify that the original array was copied. */
+    }
+
+    public static void problemFour () {
+        /*Write a program that creates an integer array with 5 elements (i.e., numbers).
+        The numbers can be any integers.
+        Print out the value at the first index and the last index using length - 1 as the index.
+        Now try printing the value at index = length ( e.g., myArray[myArray.length ] ).
+        Notice the type of exception which is produced.
+        Now try to assign a value to the array index 5.
+        You should get the same type of exception.*/
+    }
+
+    public static void problemFive () {
+        /*Write a program where you create an integer array with a length of 5.
+        Loop through the array and assign the value of the loop control variable (e.g., i) to the corresponding index in the array.*/
+    }
+
+    public static void problemSix () {
+        /*Write a program where you create an integer array of 5 numbers.
+        Loop through the array and assign the value of the loop control variable multiplied by 2 to the corresponding index in the array.*/
+    }
+
+    public static void problemSeven () {
+        /*Write a program where you create an array of 5 elements.
+        Loop through the array and print the value of each element, except for the middle (index 2) element.*/
+    }
+
+    public static void problemEight () {
+        /*Write a program that creates a String array of 5 elements and
+        swaps the first element with the middle element without creating a new array.*/
+    }
+
+    public static void problemNine () {
+        /*Write a program to sort the following int array in ascending order: {4, 2, 9, 13, 1, 0}.
+        Print the array in ascending order, print the smallest and the largest element of the array.
+        The output will look like the following:
+            Array in ascending order: 0, 1, 2, 4, 9, 13
+            The smallest number is 0
+            The biggest number is 13*/
+    }
+
+    public static void problemTen () {
+        /*Create an array that includes an integer, 3 strings, and 1 double. Print the array.*/
+    }
+
+
 }
